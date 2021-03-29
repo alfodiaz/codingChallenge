7a0e6a6a9ac17e89df56480aa50e689d8b177ac0
alfodiaz <afsdiazgramont@gmail.com>
2021-03-29 17:02:07 -0300

    Add Material Ui and React Router

--- codingchallenge/src/App.js
+++ codingchallenge/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React,{ useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,28 +9,70 @@ import {
 
 import 'fontsource-roboto';
 
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+import {Home} from "./components/home/Home";
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { useSelector, useDispatch } from 'react-redux';
 
 
-import {Home } from "./components/home/Home"
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function App() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const classes = useStyles();
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
+      <AppBar position="static">
+        <Toolbar>
+        <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Menu
+  id="simple-menu"
+  anchorEl={anchorEl}
+  keepMounted
+  open={Boolean(anchorEl)}
+  onClose={handleClose}
+>
+  <MenuItem onClick={handleClose}><Link to="/">Home</Link></MenuItem>
+  <MenuItem onClick={handleClose}><Link to="/about">About</Link></MenuItem>
+  <MenuItem onClick={handleClose}><Link to="/users">Users</Link></MenuItem>
+</Menu>
+          <Typography variant="h6" className={classes.title}>
+            Coding Challenge
+          </Typography>
+        </Toolbar>
+      </AppBar>
+     
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
--- codingchallenge/src/components/home/Home.js
+++ codingchallenge/src/components/home/Home.js
@@ -1,10 +1,51 @@
 import{
-  Button,TextField
+  Button,TextField,InputAdornment,Grid
   } 
   from '@material-ui/core';
+import ComboBox from '../Autocomplete/Combobox';
+import React, { useState, useEffect, useReducer,useMemo} from 'react';
+import {indexDB,artists} from "../../api/db/indexedDb";
+
+
+
+const initialArtists = [];
+function init(initialArtists){
+  return [];
+} 
+
+function reducer(state,action){
+  switch (action.type) {
+    case 'fill-autocomplete':
+      return artists;
+      
+    defauult:
+      return new Error();
+  }
+}
+
 
 export  function Home() {
+
+  // const [artists, setArtists] = useState([]);
+
+  const [state, dispatch] = useReducer(reducer, initialArtists, init);
+  const [loaded,setLoaded] = useState(false);
+  //const artistsMemo = useMemo(()=>artists,[]);
+
+   useEffect(()=>{
+      console.log(artists) ;
+      if(!loaded){   
+      dispatch({
+        type: 'fill-autocomplete',
+      });
+      setLoaded(true);
+    }
+  });
+
+  
+
   return (<>
+  <div style={{padding: 20+"px"}}>
     <h2>Home</h2>
     <h4>Buttons of material-ui framework</h4>
     <Button variant="contained">Default</Button>
@@ -22,7 +63,28 @@ export  function Home() {
     </Button>
     <h4> Text fields from material-ui framework</h4>
     <TextField id="outlined-basic" label="Insert text here" variant="outlined" />
+    <h4>Number text field from Material iu framework</h4>
+   <TextField
+          label="With normal TextField"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+          id="standard-number"
+     label="Price"
+     type="number"
+     InputLabelProps={{
+       shrink: true,
+     }}
+        />
+    <h4>Autocomplete example with indexDB</h4>
+    {
+      loaded == true && (<ComboBox data={state}></ComboBox>)
+
+    }
+    </div>
      </> );
+     
+    
   };
 
   
